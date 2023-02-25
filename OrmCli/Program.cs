2462ef84659911b161e6f353c0c09765f61af7f0
d4niel <olayajuandaniel8@gmail.com>
2023-02-25 18:55:47 -0500

    initial

---
+++ OrmCli/Program.cs
@@ -0,0 +1,5 @@
+﻿using SqlOrm;
+using System;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine(SqlOrm.Class1.Db());
---
+++ SqlOrm/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace SqlOrm;
+public class Class1
+{
+    public static string Msg()
+    {
+        return "Hi";
+    }
+
+    public static string Db()
+    {
+        return "This is a orm";
+    }
+}
