f5de68d47a58d527eb9e4bd8f839c7cbd0b102ec
d4niel <olayajuandaniel8@gmail.com>
2023-03-16 21:01:45 -0500

    Read

--- SqlOrm/src/Queries/Orm.queries.cs
+++ SqlOrm/src/Queries/Orm.queries.cs
@@ -3,18 +3,31 @@ using SqlOrm.Interfaces.Connection;
 using SqlOrm.Connection;
 using System.Collections;
 using System.Data.Common;
+using System.Data.SqlClient;
 
 namespace SqlOrm.Queries;
 
 
 public class QueryClient : IQueryClient
 {
-    public void Read()
+    public void Read(IClient client, string query)
     {
-        
+        using(client.getClient)
+        {
+            var command = new SqlCommand();
+            command.CommandText = query;
+            command.Connection = client.getClient;
+            client.Open();
+            var reader = command.ExecuteReader();
+            while(reader.Read())
+            {
+                return reader.GetString(0);
+            }
+            client.Close();
+        }
     }
 
-    public void Insert()
+    public void Insert(IClient client, string query)
     {
 
     }
